9c1b6497752ef43af1f77a714872efbfda728b0f
lachgar-ilyas <157811326+lachgar-ilyas@users.noreply.github.com>
2024-01-31 12:44:45 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-h
+retry = False
+
+while not retry:
+    nbr = int(input("Veuillez saisir un nombre : "))
+    print(nbr)
+
+    for i in range(11):
+        print(nbr * i)
+
+    choice = input("Voullez vous recommencer? (Y/N) ").lower()
+
+    if choice == "Y":
+        retry = False
+    elif choice == "N":
+        retry = True
+        print("FIN")
+    else:
+        print("Choix invalide. Recommencez!")
